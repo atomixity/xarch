067bb3703d8eef79aeff0cfe71e3583e24f8d294
Leesoo Ahn <lsahn@ooseel.net>
2025-03-27 20:32:50 +0900

    arch: util: refactor util header
    add a few useful types and rename ___bit4 with ___class4.
    
    Signed-off-by: Leesoo Ahn <lsahn@ooseel.net>

--- arch/util.h
+++ arch/util.h
@@ -3,19 +3,32 @@
 
 #include <stdint.h>
 
-#define ___bit4(d_bit4, c_bit4, b_bit4, a_bit4) \
-    (                                           \
-        ((0b ## d_bit4) << 12) |                \
-        ((0b ## c_bit4) <<  8) |                \
-        ((0b ## b_bit4) <<  4) |                \
-        ((0b ## a_bit4)      )                  \
+typedef int8_t      s8;
+typedef int16_t     s16;
+typedef int32_t     s32;
+typedef int64_t     s64;
+
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
+typedef uint64_t    u64;
+
+typedef void* addr_t;
+
+
+#define ___class4(class3, class2, class1, class0)   \
+    (                                               \
+        ((0b ## class3) << 12) |                    \
+        ((0b ## class2) <<  8) |                    \
+        ((0b ## class1) <<  4) |                    \
+        ((0b ## class0)      )                      \
     )
 
-#define ISA16(_instr, d4, c4, b4, a4, _mask)            \
+#define ISA16(_instr, c3, c2, c1, c0, _mask)            \
     (!!(                                                \
-        (___bit4(d4, c4, b4, a4) & ((uint16_t)(_mask))) \
+        (___class4(c3, c2, c1, c0) & ((u16)(_mask)))    \
             ==                                          \
-        ((*(uint16_t*)(_instr)) & ((uint16_t)(_mask)))  \
+        ((*(u16*)(_instr)) & ((u16)(_mask)))            \
             ? 1 : 0                                     \
     ))
 
